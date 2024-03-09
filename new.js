9b33f5ce6f55e0c6c8c7753032dce7c6bf7239cb
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:22:29 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,16 @@
+"use strict";
+let favorite_fruit = ["apple", "orange", "grapes", "bananas", "kiwi"];
+if (favorite_fruit.includes("apple")) {
+    console.log("apple");
+}
+if (favorite_fruit.includes("orange")) {
+    console.log("orange");
+}
+if (favorite_fruit.includes("grapes"))
+    (console.log("grapes"));
+if (favorite_fruit.includes("bananas")) {
+    console.log("you really like bananas");
+}
+if (favorite_fruit.includes("kiwi")) {
+    console.log("you really like kiwi");
+}
---
+++ new.ts
@@ -0,0 +1,23 @@
+let favorite_fruit: string[] = ["apple","orange","grapes","bananas","kiwi"]
+
+if (favorite_fruit.includes("apple")){
+    console.log("apple")
+}
+
+
+if  (favorite_fruit.includes("orange")){
+    console.log("orange")
+}
+
+
+if (favorite_fruit.includes("grapes"))(
+    console.log("grapes")
+)
+
+if (favorite_fruit.includes("bananas")){
+    console.log("you really like bananas")
+}
+
+if (favorite_fruit.includes("kiwi")){
+    console.log("you really like kiwi")
+}
